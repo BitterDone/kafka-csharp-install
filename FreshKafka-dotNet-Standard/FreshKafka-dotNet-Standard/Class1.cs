c9ca267792470e234fd70776bd1462cf47a4190c
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-10-26 10:20:06 -0500

    Sample library and unit test

---
+++ FreshKafka-dotNet-Standard/FreshKafka-dotNet-Standard/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FreshKafka_dotNet_Standard
+{
+	public class Class1
+	{
+		public static void print(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public static bool isValidString(string message)
+		{
+			return null != message && message.Length > -1;
+		}
+	}
+}
---
+++ FreshKafka-dotNet-Standard/SampleLibraryTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleLibraryTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.IsTrue(FreshKafka_dotNet_Standard.Class1.isValidString(""));
+			Assert.IsFalse(FreshKafka_dotNet_Standard.Class1.isValidString(null));
+		}
+	}
+}
